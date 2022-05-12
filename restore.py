e865d75d79a403a17bc9ed205bedc64fd115d4a3
Reinier Romero Mir <nenirey@gmail.com>
2022-05-12 11:30:07 -0400

    add restore from postgresql db

--- restore.py
+++ restore.py
@@ -5,11 +5,38 @@ from dropbox.files import WriteMode
 from dropbox.exceptions import ApiError, AuthError
 import zipfile
 import urllib.parse
+import psycopg2
 
+DATABASE_URL = os.getenv('DATABASE_URL')
 DBXTOKEN = os.getenv('DBXTOKEN')
 APP_KEY = os.getenv('APP_KEY')
 ADDR = os.getenv('ADDR')
 botzipdb = './'+urllib.parse.quote(ADDR, safe = '')+'.zip'
+
+
+def db_init():
+    try:
+       con = psycopg2.connect(DATABASE_URL)
+       cur = con.cursor()
+       cur.execute("SELECT * FROM information_schema.tables WHERE table_name=%s", ('simplebot_db',))
+       if bool(cur.rowcount):
+          print("La tabla existe, restaurando...")
+          cur.execute("SELECT id, name, data FROM simplebot_db")
+          simple_data = cur.fetchone()
+          open(botzipdb, 'wb').write(simple_data[2])
+       else:
+          print("La tabla no existe, creando...")
+          cur.execute("CREATE TABLE simplebot_db (id bigint PRIMARY KEY, name TEXT, data BYTEA)")
+          con.commit()
+          cur.execute("ALTER TABLE simplebot_db ALTER COLUMN data SET STORAGE EXTERNAL")
+          con.commit()
+       cur.close()
+    except Exception as error:
+       print('Cause: {}'.format(error))
+    finally:
+       if con is not None:
+           con.close()
+           print('Database connection closed.')
 
 
 def unzipfile(file_path, dir_path):
@@ -37,7 +64,11 @@ def restore(backup_path):
        code = str(sys.exc_info())
        print(code)
 
-if DBXTOKEN:
+if DATABASE_URL:
+   db_init()
+   if os.path.isfile(botzipdb):
+      unzipfile(botzipdb,'/')
+elif DBXTOKEN:
    if APP_KEY:
       dbx = dropbox.Dropbox(oauth2_refresh_token=DBXTOKEN, app_key=APP_KEY)
    else:
@@ -51,3 +82,4 @@ if DBXTOKEN:
    except AuthError:
        sys.exit("ERROR: Invalid access token; try re-generating an "
                 "access token from the app console on the web.")
+
